be1b6c8b207be09b22f6a05eddf77afbc3faae3a
cristopherdiaz7 <crl.diaz@alumno.um.edu.ar>
2025-11-11 11:26:53 -0300

    chore(deploy): add PyMySQL and make ventas import robust

--- ventas/__init__.py
+++ ventas/__init__.py
@@ -1,2 +1,8 @@
-import pymysql
-pymysql.install_as_MySQLdb()
+try:
+	import pymysql
+	pymysql.install_as_MySQLdb()
+except Exception:
+	# PyMySQL not installed in the environment; if mysqlclient is available
+	# we'll use it instead. This keeps containers working when PyMySQL isn't
+	# provided but mysqlclient (native) is present.
+	pass
