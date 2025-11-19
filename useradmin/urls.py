dd96bff66ae524093ee9e9aca4b200e9a483f91e
cristopherdiaz7 <crl.diaz@alumno.um.edu.ar>
2025-11-19 14:31:26 -0300

    tp10

--- useradmin/urls.py
+++ useradmin/urls.py
@@ -1,7 +1,15 @@
 # useradmin/urls.py
 
 from django.urls import path
-from .views import login_view, logout_view, register_view, UsuarioListView, UsuarioDetailView, me_view
+from .views import (
+    login_view,
+    logout_view,
+    register_view,
+    UsuarioListView,
+    UsuarioDetailView,
+    me_view,
+    change_password_view,
+)
 
 urlpatterns = [
     path('login/', login_view, name='login'),
